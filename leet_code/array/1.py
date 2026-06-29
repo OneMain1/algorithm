aca493eb6fa8536c90f218aaf38a8bad967dbffd
OneMain1 <shindong97411@gmail.com>
2026-06-29 22:52:39 +0900

    add 1.py

---
+++ leet_code/array/1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                candidate = nums[i] + nums[j]
+                if candidate == target:
+                    return [i,j]
+                
+# Solution 1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
